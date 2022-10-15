ced2fab8a48de3ef64232b293bbb9b2facddc178
dav1perLAPTOK <wlodar.igor@gmail.com>
2022-10-15 18:24:48 +0200

    serializer item

--- api/trasher_api/trasher/serializers.py
+++ api/trasher_api/trasher/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 from .models import User, Item
 
+
 class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         return User.objects.create(
@@ -25,13 +26,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__' 
 
+
 class ItemSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
-        return User.objects.create(
+        return Item.objects.create(
             name=validated_data.get('name'),
             latitude=validated_data.get('latitude'),
             longitude=validated_data.get('longitude'),
-            score=validated_data.get('score'),
+            user=validated_data.get('user'),
+            status=validated_data.get('status'),
+            category=validated_data.get('category'),
+            image=validated_data.get('image'),
+            likes=validated_data.get('likes'),
+            creation_date=validated_data.get('creation_date'),
+            last_updated=validated_data.get('last_updated')
         )
 
     def update(self, instance, validated_data):
@@ -40,10 +48,17 @@ class ItemSerializer(serializers.ModelSerializer):
         instance.latitude = validated_data.get(
             'latitude', instance.latitude)
         instance.longitude = validated_data.get('longitude', instance.longitude)
-        instance.score = validated_data.get('score', instance.score)
+        instance.user = validated_data.get('user', instance.user)
+        instance.status = validated_data.get('status', instance.status)
+        instance.category = validated_data.get('category', instance.category)
+        instance.image = validated_data.get('image', instance.image)
+        instance.likes = validated_data.get('likes', instance.likes)
+        instance.creation_date = validated_data.get('creation_date', instance.creation_date)
+        instance.last_updated = validated_data.get('last_updated', instance.last_updated)
         instance.save()
         return instance
 
     class Meta:
-        model = User
-        fields = '__all__' 
+        model = Item
+        fields = '__all__'
+
