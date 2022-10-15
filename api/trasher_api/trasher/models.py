e88665ca60bea1c67bed253584cedd8000289a20
Tomasz Rzymyszkewicz <tomek.rzymyszkiewcz@gmail.com>
2022-10-15 18:10:23 +0000

    Images saving

--- api/trasher_api/trasher/models.py
+++ api/trasher_api/trasher/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+import os
+from uuid import uuid4
+
+def path_and_rename(path):
+    def wrapper(instance, filename):
+        ext = filename.split('.')[-1]
+        # get filename
+        if instance.pk:
+            filename = '{}.{}'.format(instance.pk, ext)
+        else:
+            # set filename as random string
+            filename = '{}.{}'.format(uuid4().hex, ext)
+        # return the whole path to the file
+        return os.path.join(path, filename)
+    return wrapper
 
 
 class User(models.Model):
@@ -42,7 +57,7 @@ class Item(models.Model):
         max_length=30,
         null=True
     )
-    image = models.ImageField()
+    image = models.ImageField(upload_to=path_and_rename('static/images'))
     likes = models.IntegerField(null=True)
     views = models.IntegerField(null=True)
     creation_date = models.DateTimeField(
--- api/trasher_api/trasher/urls.py
+++ api/trasher_api/trasher/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
   path('api/user/', views.UserView.as_view()),
   path('api/user/<int:id>/', views.UserView.as_view()),
-  path('api/user/', views.ItemView.as_view()),
-  path('api/user/<int:id>/', views.ItemView.as_view()),
+  path('api/item/', views.ItemView.as_view()),
+  path('api/item/<int:id>/', views.ItemView.as_view()),
 ]
