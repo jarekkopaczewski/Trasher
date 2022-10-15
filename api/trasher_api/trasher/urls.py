6193577e9ad0848479d536dd11f75d9a5b8c0a43
Tomasz Rzymyszkewicz <tomek.rzymyszkiewcz@gmail.com>
2022-10-15 14:56:52 +0000

    Initial API commit

---
+++ api/trasher_api/trasher/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+#   path('api/item/', views.PatientView.as_view()),
+#   path('api/item/<int:id>/', views.PatientView.as_view()),
+#   path('api/user/', views.DoctorView.as_view()),
+#   path('api/user/<int:id>/', views.DoctorView.as_view()),
+]
